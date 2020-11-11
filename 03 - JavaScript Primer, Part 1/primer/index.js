f723d3922c2dafba314ccf3be7912cd0a719ca1b
tult_tt <tult_tt@rikkeisoft.com>
2020-11-11 17:36:04 +0700

    update source code chappter 3

---
+++ 03 - JavaScript Primer, Part 1/primer/index.js
@@ -0,0 +1,17 @@
+let hatPrice = 100
+console.log(`Hat price: ${hatPrice}$`)
+let bootsPrice = '100'
+console.log(`Boots price: ${bootsPrice}$`)
+if (hatPrice == bootsPrice) {
+  console.log('Prices are the same')
+} else {
+  console.log('Prices are different')
+}
+
+let totalPrice = hatPrice + bootsPrice
+console.log(`Total Price: ${totalPrice}`)
+
+let myvariable = 'Adam'
+console.log(`Type: ${typeof myvariable}`)
+myvariable = 100
+console.log(`Type: ${typeof myvariable}`)
---
+++ 03 - JavaScript Primer, Part 1/primer/lesson.md
@@ -0,0 +1,12 @@
+## Javascript types
+
+#### JavaScript have six built-in types included:
+
+1. number
+2. string
+3. null: used to indicate a nonexistent or invalid reference.
+4. undefined: variable has been defined but not been assigned a value.
+5. boolean
+6. symbol: used to represent unique constant values, such as **keys** in collections.
+
+> In JavaScript, values have types, not variables.
