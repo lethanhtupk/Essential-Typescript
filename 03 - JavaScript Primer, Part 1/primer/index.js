2dfce6adc7c4b1a8219fe26dcd04dda8d4bfab0c
tult_tt <tult_tt@rikkeisoft.com>
2020-11-13 17:32:14 +0700

    update code

--- 03 - JavaScript Primer, Part 1/primer/index.js
+++ 03 - JavaScript Primer, Part 1/primer/index.js
@@ -15,3 +15,8 @@ let myvariable = 'Adam'
 console.log(`Type: ${typeof myvariable}`)
 myvariable = 100
 console.log(`Type: ${typeof myvariable}`)
+
+let firstCity
+// if firstCity is null or undefined, then the secondCity will fallback to "Hanoi"
+let secondCity = firstCity || 'Hanoi'
+console.log(`City: ${secondCity}`)
