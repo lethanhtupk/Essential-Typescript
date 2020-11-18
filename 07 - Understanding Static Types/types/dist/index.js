86aea343f4419fca677e69c51a1665d6e7ed6bb4
tult_tt <tult_tt@rikkeisoft.com>
2020-11-18 17:55:15 +0700

    update code for chappter 7

---
+++ 07 - Understanding Static Types/types/dist/index.js
@@ -0,0 +1,36 @@
+// In Javascript, values has type, NOT variables
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 'Hello';
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
+console.log('----------------------------');
+// Type Annotation
+function calculateTax(amount, format) {
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
+}
+// The type of taxNumber and taxString IS string | number type. NOT string nor number.
+// let taxNumber: string | number = calculateTax(100, false)
+// let taxString: string | number = calculateTax(100, true)
+// To tell TypeScript compiler to TREAT a VALUE as SPECIFIC TYPE
+// USING Type Assertions
+let taxNumber = calculateTax(100, false);
+let taxString = calculateTax(100, true);
+// can only use the METHODS and PROPERTIES that exist on BOTH 2 types.
+console.log(`Number Value: ${taxNumber.toFixed(2)}`);
+console.log(`String Value: ${taxString.charAt(0)}`);
+// let price = 100
+// let taxAmount = calculateTax(price)
+// let halfShare = taxAmount / 2
+// let personVal = calculateTax('Bob')
+// console.log(`Price: ${price}`)
+// console.log(`Full amount in tax: ${taxAmount}`)
+// console.log(`Half share: ${halfShare}`)
+// console.log(`Name: ${personVal}`)
+// console.log(`${12} = ${calculateTax(12)}`)
+// console.log(`${'Hello'} = ${calculateTax('Hello')}`)
+// console.log(`${true} = ${calculateTax(true)}`)
